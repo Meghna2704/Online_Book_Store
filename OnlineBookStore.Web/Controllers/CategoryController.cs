3671209d451635840802f88ed12538a733348da8
Meghna Verma <meghnav274@gmail.com>
2024-10-01 21:32:33 +0530

    CRUD Operations

---
+++ OnlineBookStore.Web/Controllers/CategoryController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookStore.Web.Data;
+using OnlineBookStore.Web.Models;
+
+namespace OnlineBookStore.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+        public IActionResult Index()
+        {
+            List<Category> categoryList =  _db.Categories.ToList();
+            return View(categoryList);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display order cannot exactly match the name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _db.Categories.FirstOrDefault(x => x.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            if(id != null || id != 0)
+            {
+                Category obj = _db.Categories.FirstOrDefault(x => x.Id == id);
+                if(obj != null)
+                {
+                    _db.Categories.Remove(obj);
+                    _db.SaveChanges();
+                    TempData["success"] = "Category deleted successfully!";
+                    return RedirectToAction("Index");
+                }
+                else { return NotFound(); }
+            }
+            else
+                return NotFound();
+        }
+    }
+}
---
+++ OnlineBookStore.Web/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBookStore.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,255, ErrorMessage ="The Display order must be between 1-255")]
+        public int DisplayOrder { get; set; }
+    }
+}
